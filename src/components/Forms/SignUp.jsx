fa1158ece54ae6a75bb1eda577e53841b0d954a4
N10T <andy.tisba@gmail.com>
2020-03-05 12:19:24 +0100

    Deploying conflict CORS

--- src/components/Forms/SignUp.jsx
+++ src/components/Forms/SignUp.jsx
@@ -1,7 +1,6 @@
-
 // React
-import React, {useState} from "react";
-import APIHandler from './../../api/APIHandler';
+import React, { useState } from "react";
+import APIHandler from "./../../api/APIHandler";
 
 // Style
 import TextField from "@material-ui/core/TextField";
@@ -25,7 +24,7 @@ const useStyles = makeStyles(theme => ({
   }
 }));
 
-export default function SignUp() {
+export default function SignUp({ history }) {
   const classes = useStyles();
 
   const [formValues, setFormValues] = useState({});
@@ -35,18 +34,18 @@ export default function SignUp() {
   const handleInputs = e => {
     const value = e.target.value;
     const name = e.target.name;
-    setFormValues({ ...formValues, [name]: value});
+    setFormValues({ ...formValues, [name]: value });
   };
 
   const handleImage = e => {
     setAvatar(e.target.files[0]);
-    const tmpUrl =URL.createObjectURL(e.target.files[0]);
+    const tmpUrl = URL.createObjectURL(e.target.files[0]);
     setTmpAvatar(tmpUrl);
   };
 
   const handleSubmit = e => {
     e.preventDefault();
-    console.log(formValues)
+    console.log(formValues);
     const data = new FormData();
     data.append("name", formValues.name || "");
     data.append("lastname", formValues.lastname || "");
@@ -54,13 +53,18 @@ export default function SignUp() {
     data.append("key", formValues.key || "");
     data.append("password", formValues.password || "");
     data.append("avatar", avatar);
-    
-    api.post('/auth/signup', data, setTimeout(window.location.href = `/signin`,250)).then(res => {
-      console.log(res.data)
-    }).catch(err => {
-      console.log(err.response)
-    })  
-  }
+
+    api
+      .post("/auth/signup", data)
+      .then(res => {
+        console.log(res.data);
+        history.push("/signin");
+        // setTimeout(window.location.href = `/signin`,250)
+      })
+      .catch(err => {
+        console.log(err.response);
+      });
+  };
 
   return (
     <div className="user-form">
@@ -71,67 +75,65 @@ export default function SignUp() {
         onSubmit={handleSubmit}
         onChange={handleInputs}
       >
-          <AvatarUser
-          tmpAvatar={tmpAvatar} clbk={handleImage} 
-          />
-          <TextField
-            required="true"
-            id="outlined-basic"
-            label="Name"
-            variant="outlined"
-            name="name"
-            />
-          <TextField
+        <AvatarUser tmpAvatar={tmpAvatar} clbk={handleImage} />
+        <TextField
+          required="true"
+          id="outlined-basic"
+          label="Name"
+          variant="outlined"
+          name="name"
+        />
+        <TextField
           required="true"
           id="outlined-basic"
           label="Lastname"
           variant="outlined"
           name="lastname"
-          />
-          <TextField
+        />
+        <TextField
           type="email"
           required="true"
           id="outlined-basic"
           label="E-mail"
           variant="outlined"
           name="email"
-          />
-          <TextField
+        />
+        <TextField
           id="outlined-basic"
           label="Your building key"
           variant="outlined"
           name="key"
-          />
-          <TextField
+        />
+        <TextField
           type="password"
           required="true"
           id="outlined-basic"
           label="Password"
           variant="outlined"
           name="password"
-          />
-          <div className="one-column">
-            <Button
-              type="submit"
-              variant="contained"
-              color="primary"
-              className={classes.button}
-              size="large"
-              // endIcon={<Icon>send</Icon>}
-            >
+        />
+        <div className="one-column">
+          <Button
+            type="submit"
+            variant="contained"
+            color="primary"
+            className={classes.button}
+            size="large"
+            // endIcon={<Icon>send</Icon>}
+          >
             sign up
-            </Button>
-            <Button
-              variant="contained"
-              color="secondary"
-              className={classes.button}
-              size="small"
-              href="/signin"
-              //   endIcon={<Icon>send</Icon>}
-            >
-              sign in
-            </Button>
-          </div>
+          </Button>
+          <Button
+            variant="contained"
+            color="secondary"
+            className={classes.button}
+            size="small"
+            href="/signin"
+            //   endIcon={<Icon>send</Icon>}
+          >
+            sign in
+          </Button>
+        </div>
       </form>
     </div>
   );

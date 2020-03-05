d1f6717c7a9985992b253b5a323d4b038759651c
N10T <andy.tisba@gmail.com>
2020-03-05 14:55:01 +0100

    React Links | OK

--- src/components/FooterMain.jsx
+++ src/components/FooterMain.jsx
@@ -1,17 +1,20 @@
 //React
-import React from 'react';
-
+import React,{ useState, useEffect, useContext } from 'react';
+import {useAuth} from './../auth/useAuth'
 // Style
 import { makeStyles } from '@material-ui/core/styles';
 import BottomNavigation from '@material-ui/core/BottomNavigation';
 import BottomNavigationAction from '@material-ui/core/BottomNavigationAction';
-import InfoIcon from '@material-ui/icons/Info';
+// import InfoIcon from '@material-ui/icons/Info';
 import HomeIcon from '@material-ui/icons/Home';
 import AddCircleIcon from '@material-ui/icons/AddCircle';
 import ApartmentIcon from '@material-ui/icons/Apartment';
 
 // Component
 import MessageBadge from './Badge/MessageBadge';
+import AccountCircleIcon from '@material-ui/icons/AccountCircle';
+import { Link as RouterLink } from 'react-router-dom';
+import Link from '@material-ui/core/Link';
 
 const useStyles = makeStyles({
   root: {
@@ -22,22 +25,23 @@ const useStyles = makeStyles({
 });
 
 export default function LabelBottomNavigation() {
+  const { currentUser, isLoggedIn, isLoading} = useAuth();
   const classes = useStyles();
   const [value, setValue] = React.useState('recents');
-
+  
   const handleChange = (event, newValue) => {
     setValue(newValue);
   };
-
+  
   return (
     
     <BottomNavigation id="footer" value={value} onChange={handleChange} className={classes.root}>
-      <BottomNavigationAction href="/" label="" value="home" icon={<HomeIcon />} />
-      <BottomNavigationAction href="/admin/building" label="" value="infos" icon={<InfoIcon />} />
-      <BottomNavigationAction href="/building/create-information" label="" value="New post" icon={<AddCircleIcon />} />
-      <BottomNavigationAction href="/user/messages" value="Messages" icon={<MessageBadge />} />
-      {/* <BottomNavigationAction label="Profile" value="profile" icon={<AccountCircle />} /> */}
-      <BottomNavigationAction href="/user/building" label="" value="My cocoon" icon={<ApartmentIcon />} />
+      <Link to="/" component={RouterLink}><BottomNavigationAction label="Home" value="home" icon={<HomeIcon />} /></Link>
+      <Link to={`/profile/${currentUser._id}`} component={RouterLink}><BottomNavigationAction label="Profile" value="profile" icon={<AccountCircleIcon />} /></Link>
+      {/* <BottomNavigationAction href="/admin/building" label="My profile" value="profile" icon={<AccountCircleIcon />} /> */}
+      <Link to="/building/create-information" component={RouterLink}><BottomNavigationAction label="" value="New post" icon={<AddCircleIcon />} /></Link>
+      <Link to="/user/messages" component={RouterLink}><BottomNavigationAction value="Messages" icon={<MessageBadge />} /></Link>
+      <Link to="/user/building" component={RouterLink}><BottomNavigationAction label="" value="My cocoon" icon={<ApartmentIcon />} /></Link>
     </BottomNavigation>
   );
 }
